35bf67b3e87d6f266b3210270d1d11830fad11ee
Cihat Solak <cihatsolak@hotmail.com>
2023-03-01 23:01:08 +0300

    feat: creation of payment service

---
+++ PaymentService.API/Consumers/StockReservedEventConsumer.cs
@@ -0,0 +1,47 @@
+﻿namespace PaymentService.API.Consumers
+{
+    public class StockReservedEventConsumer : IConsumer<StockReservedEvent>
+    {
+        private readonly ILogger<StockReservedEventConsumer> _logger;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public StockReservedEventConsumer(
+            ILogger<StockReservedEventConsumer> logger,
+            IPublishEndpoint publishEndpoint)
+        {
+            _logger = logger;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task Consume(ConsumeContext<StockReservedEvent> context)
+        {
+            var balance = 3000m; //simulasyon
+
+            if (balance < context.Message.Payment.TotalPrice)
+            {
+                _logger.LogInformation("{@totalPrice} TL was not withdrawn from credit card for user id={@buyerId}", context.Message.Payment.TotalPrice, context.Message.BuyerId);
+
+                PaymentFailedEvent paymentFailedEvent = new()
+                {
+                    BuyerId = context.Message.BuyerId,
+                    OrderId = context.Message.OrderId,
+                    Message = "Not enough balance",
+                    OrderItems = context.Message.OrderItems
+                };
+
+                await _publishEndpoint.Publish(paymentFailedEvent);
+                return;
+            }
+
+            _logger.LogInformation("{@totalPrice} TL was withdrawn from credit card for user id = {@buyerId}", context.Message.Payment.TotalPrice, context.Message.BuyerId);
+
+            PaymentCompletedEvent paymentCompletedEvent = new()
+            {
+                BuyerId = context.Message.BuyerId,
+                OrderId = context.Message.OrderId
+            };
+
+            await _publishEndpoint.Publish(paymentCompletedEvent);
+        }
+    }
+}
--- PaymentService.API/Program.cs
+++ PaymentService.API/Program.cs
@@ -1,13 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMassTransit(busRegistrationConfigurator =>
+{
+    busRegistrationConfigurator.AddConsumer<StockReservedEventConsumer>();
+
+    busRegistrationConfigurator.UsingRabbitMq((busRegistrationContext, rabbitMqBusFactoryConfigurator) =>
+    {
+        rabbitMqBusFactoryConfigurator.Host(builder.Configuration["RabbitMqSetting:HostAddress"], "/", hostConfigurator =>
+        {
+            hostConfigurator.Username(builder.Configuration["RabbitMqSetting:Username"]);
+            hostConfigurator.Password(builder.Configuration["RabbitMqSetting:Password"]);
+        });
+
+        rabbitMqBusFactoryConfigurator.ReceiveEndpoint(RabbitQueueName.StockReservedEventQueueName, endpoint =>
+        {
+            endpoint.ConfigureConsumer<StockReservedEventConsumer>(busRegistrationContext);
+        });
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,30 +32,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+await app.RunAsync();
