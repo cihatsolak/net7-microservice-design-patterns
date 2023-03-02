8c90807dba7c099ed30a6882000a51ce333b1406
Cihat Solak <cihatsolak@hotmail.com>
2023-03-02 23:23:32 +0300

    feat: state context configuration

--- Orchestration/Orchestration.StateMachineWorkerService/Usings.cs
+++ Orchestration/Orchestration.StateMachineWorkerService/Usings.cs
@@ -1,5 +1,9 @@
 ﻿global using MassTransit;
+global using MassTransit.EntityFrameworkCoreIntegration;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.EntityFrameworkCore.Metadata.Internal;
 global using Orchestration.StateMachineWorkerService;
+global using Orchestration.StateMachineWorkerService.State;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using System.Text;
