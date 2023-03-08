0aa9ff98f445390571498f4a5d1d4b3e2328b72c
Cihat Solak <cihatsolak@hotmail.com>
2023-03-08 21:33:38 +0300

    feat: create main stream class

--- EventSourcing/EventSourcing.API/Using.cs
+++ EventSourcing/EventSourcing.API/Using.cs
@@ -1,2 +1,6 @@
 ﻿global using EventSourcing.API.Models;
+global using EventSourcing.Shared.Events;
+global using EventStore.ClientAPI;
 global using MediatR;
+global using System.Text;
+global using System.Text.Json;
