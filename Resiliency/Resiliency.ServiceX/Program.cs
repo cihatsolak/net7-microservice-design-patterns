899d9780f254febdc232989daf8001266233b029
Cihat Solak <cihatsolak@hotmail.com>
2023-03-11 16:00:14 +0300

    feat: apply retry pattern

--- Resiliency/Resiliency.ServiceX/Program.cs
+++ Resiliency/Resiliency.ServiceX/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpClient<ProductService>(opt =>
 {
     opt.BaseAddress = new Uri("https://localhost:7202/api/products/");
-});
+}).AddPolicyHandler(GetRetryPolicy());
 
 var app = builder.Build();
 
@@ -21,3 +21,20 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
+
+
+IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound).WaitAndRetryAsync(5, retryAttempt =>
+    {
+        Debug.WriteLine($"Retry Count :{retryAttempt}");
+        return TimeSpan.FromSeconds(10);
+    }, onRetryAsync: OnRetryAsync);
+}
+
+Task OnRetryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
+{
+    Debug.WriteLine($"Request is made again:{arg2.TotalMilliseconds}");
+
+    return Task.CompletedTask;
+}
